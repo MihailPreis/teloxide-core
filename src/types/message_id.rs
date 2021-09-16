3d468564d5d63330d08740e2787d0f09b67eaaea
Mihail Preis <mihail.preis@gmail.com>
2021-09-16 19:52:10 +0500

    Update message_id.rs

--- src/types/message_id.rs
+++ src/types/message_id.rs
@@ -1,4 +1,7 @@
+use serde::{Deserialize, Serialize};
+
 /// This object represents a unique message identifier.
+#[derive(Clone, Debug, Eq, Hash, PartialEq, Serialize, Deserialize)]
 pub struct MessageId {
     /// Unique message identifier
     pub message_id: i32,
